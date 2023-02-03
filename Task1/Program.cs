1866b6804374b81d9f545cb64631bb82f036a8fe
semen <semen.dorohov@yandex.ru>
2023-02-03 23:37:58 +0300

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Показать двумерный массив размером m×n заполненный вещественными числами
+
+double[,] a = new double[3, 3];
+Random random = new Random();
+for (int i = 0; i < 3; i++)
+{
+    for (int j = 0; j < 3; j++)
+    {
+        a[i, j] = random.NextDouble() * 100; // NextDouble() дает случайное вещественное число в диапазоне от 0 до 1
+        Console.Write("{0,6:F2}", a[i, j]);
+    }
+    Console.WriteLine();
+}
